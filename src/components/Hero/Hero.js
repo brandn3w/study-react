af128fc81414b6a5d60c14043e635f9b6a29d3dc
Nadja Brandys <nadiusza@gmail.com>
2020-02-27 19:34:49 +0100

    Add Column component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-
-const Hero =()=> (
+const Hero = props => (
         <header className={styles.component}>                 
-                  <h2 className={styles.subtitle}>Things to do</h2> 
-                  <img className={styles.image} src='src/components/Hero/space.png'/>
+                  <h2 className={styles.subtitle}>{props.titleText}</h2> 
+                  <img className={styles.image} src={props.image}/>
       </header>
 );
 
+Hero.propTypes = {
+      titleText: PropTypes.node,
+    };
 export default Hero;
