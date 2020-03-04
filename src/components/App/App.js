befc17085a18bdfc68f89ccbd481dba44e6f0c0c
Nadja Brandys <nadiusza@gmail.com>
2020-03-04 18:38:28 +0100

    Add new files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js';
+//import List from '../List/List.js';
 import { pageContents, listData } from '../../data/dataStore';
 
 
@@ -11,7 +11,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        {/*}
         <List {...listData} />
+    */}
       </main>
     );
   }
