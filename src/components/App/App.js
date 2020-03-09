750d633db40f056230b54b47e91c1323c14a8b7f
Nadja Brandys <nadiusza@gmail.com>
2020-03-09 20:30:36 +0100

    Add search function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
-import { pageContents, listData } from '../../data/dataStore';
+//import { pageContents, listData } from '../../data/dataStore';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
 
 class App extends React.Component {
   static propTypes = {
@@ -16,8 +17,9 @@ class App extends React.Component {
     return (
 
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
         {lists.map(listData => (
   <List key={listData.id} {...listData} />
 ))}
