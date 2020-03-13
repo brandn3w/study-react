d96c24498d7f81833664035dbeb75ef375f43cc5
Nadja Brandys <nadiusza@gmail.com>
2020-03-13 22:18:09 +0100

    Site opening on heroku correctly

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styles from '../Header/Header';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon'
-import {settings} from '../../data/dataStore';
+import { settings } from '../../data/dataStore';
 
 
 class Header extends React.Component {
 
 
-    render () {
-        const {icon} = settings.header; 
-        return(
-    <header className={styles.component}>
-        <Container>
-            <div className={styles.wrapper}>
-<Link to='/' className={StyleSheetList.logo}>
-    <Icon name={icon} />
-</Link>
-<nav>
-<NavLink exact to='/'>Home</NavLink>
-<NavLink exact to='/info'>Info</NavLink>
-<NavLink exact to='/faq'>FAQ</NavLink>
-</nav>
-            </div>
-        </Container>
-        </header>
+    render() {
+        const { icon } = settings.header;
+        return (
+            <header className={styles.component}>
+                <Container>
+                    <div className={styles.wrapper}>
+                        <Link to='/' className={StyleSheetList.logo}>
+                            <Icon name={icon} />
+                        </Link>
+                        <nav>
+                            <NavLink exact to='/'>Home</NavLink>
+                            <NavLink exact to='/info'>Info</NavLink>
+                            <NavLink exact to='/faq'>FAQ</NavLink>
+                        </nav>
+                    </div>
+                </Container>
+            </header>
         );
-        }
+    }
 }
 export default Header;
